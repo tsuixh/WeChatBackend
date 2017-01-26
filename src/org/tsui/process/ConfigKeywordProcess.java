014d4033ce474f4be8c7ef2b04287d8815318fd1
TsuiXh <smartcyh@hotmail.com>
2017-01-26 16:33:47 +0800

    添加了添加关键词回复的处理逻辑

---
+++ src/org/tsui/process/ConfigKeywordProcess.java
@@ -0,0 +1,22 @@
+package org.tsui.process;
+
+import org.tsui.entity.Article;
+
+/**
+ * @author TsuiXh
+ *	负责添加关键词对应回复的类
+ */
+public class ConfigKeywordProcess {
+	
+	/**
+	 * 将图文和关键词建立关联
+	 * @param article			图文
+	 * @param keywords	关键字长串
+	 * @return					添加是否成功
+	 */
+	public static boolean addArticleReplyProcess(Article article, String keywords) {
+		boolean isSucess = false;
+		
+		return isSucess;
+	}
+}
--- src/org/tsui/servlets/ConfigServlet.java
+++ src/org/tsui/servlets/ConfigServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.tsui.entity.Article;
+
 /**
  * @author TsuiXh
  *	用来配置关键词回复
@@ -16,7 +18,23 @@ public class ConfigServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//获取回复类型
+		String reply_type = request.getParameter("reply_type");
 		
+		if ("article".equals(reply_type)) {
+			//添加图文回复
+			String title = request.getParameter("title");
+			String description = request.getParameter("description");
+			String picUrl = request.getParameter("pic_url");
+			String url = request.getParameter("url");
+			String keyword = request.getParameter("keyword");
+			Article article = new Article(title, description, picUrl, url);
+			
+		} else if ("text".equals(reply_type)) {
+			//添加文字回复
+		} else if ("default".equals(reply_type)) {
+			//设置默认回复
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
